8ea24880a7999c4dafd5c1a1751b5ee859e3f6d5
jack <jack.nguyen@weareday.one>
2023-03-31 16:02:55 +0700

    chores: remove old infrastructure

--- backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import voiceGiftRoute from './voiceGift.route.js';
-
-const router = express.Router();
-
-router.use('/v1/voice-gifts', voiceGiftRoute);
-
-export default router;
